4577f8653326c775d4389b7d10b7ca5e3845a2bb
Luiz <luiz_e_assuncao@estudante.sc.senai.br>
2023-03-15 08:14:50 -0300

    ds

--- js.js
+++ js.js
@@ -1,16 +1,18 @@
 
 let nome_cidade = []
-let res = document.getElementById('res').innerHTML
+
 
 
 function inserir(){
     let cidade = document.getElementById('cidade').value
     nome_cidade.push(cidade)
   console.log(nome_cidade)
+    let res = document.getElementById('res').innerHTML
 }
-    
+
 function excluir(){
     let cidade = document.getElementById('cidade').value
     nome_cidade.pop(cidade)
   console.log(nome_cidade)
+    let res = document.getElementById('res').innerHTML   
 }
